587871383630fc1c71d78d21c5c2197b1d7c3888
noire-er <xzlq9523@leeds.ac.uk>
2026-02-21 13:15:53 +0000

    validation changes

--- src/Main.kt
+++ src/Main.kt
@@ -1,4 +1,5 @@
 // want to add basic validation errors for website
+
 fun main() {
     val email = "tester@example.com"
     if (!email.contains()"@)) {
@@ -6,8 +7,21 @@ fun main() {
     } else {
         println("Email is valid")
     }
+// this is for missing '@' in an email address
+// Length validation
+    if (name.length < 2) {
+        println("Your name must be at least 2 letters!)
+    } else {
+        println("Name is valid")
+    }
 
+    if (surname.length < 2) {
+        println("Your surname must be at least 2 letters!)
+    } else {
+        println("surname is valid")
+    }
 
+// Numbers in name validation
 
 }
 
