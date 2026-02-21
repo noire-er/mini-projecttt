502a2e56c63f8567acc16d6c1b3fe28b51e2dc24
e. <150349678+ekunwill25@users.noreply.github.com>
2026-02-21 16:58:55 +0000

    added more validation changes for email + password

--- Main.kt
+++ Main.kt
@@ -10,18 +10,87 @@ fun main() {
 // this is for missing '@' in an email address
 // Length validation
     if (name.length < 2) {
-        println("Your name must be at least 2 letters!)
+        println("Your name must be at least 2 letters!")
     } else {
         println("Name is valid")
     }
 
     if (surname.length < 2) {
-        println("Your surname must be at least 2 letters!)
+        println("Your surname must be at least 2 letters!")
     } else {
-        println("surname is valid")
+        println("Surname is valid")
     }
 
 // Numbers in name validation
+    if (name.any { it.isDigit() }) {
+        println("Your name cannot contain numbers!")
+    } else {
+        println("Name is valid")
+    }
+
+// Numbers in surname validation
+    if (surname.any { it.isDigit() }) {
+        println("Your surname cannot contain numbers!")
+    } else {
+        println("Surname is valid")
+    }
+
+// Validate password not being empty
+    if (password.isEmpty()) {
+        println("Password cannot be empty!")
+    } else {
+        println("Password is valid")
+    }
+
+// Validate password length
+    if password.length < 8) {
+        println("Password must be at least 8 characters long!")
+    } else {
+        println("Password is valid")
+    }
+
+// Validate email containing "."
+    if (!email.contains(".")) {
+        println("This is an invalid email address: missing '.'")
+    } else {
+        println("Email is valid")
+    }
+
+// Validate email not being empty
+    if (email.isEmpty()) {
+        println("Email cannot be empty!")
+    } else {
+        println("Email is valid")
+    }
+
+//  Validate password containing an uppercase letter
+    if (!password.any { it.isUpperCase() }) {
+        println("Password must contain at least one uppercase letter!")
+    } else {
+        println("Password is valid")
+    }
+
+// Validate password containing a lowercase letter
+    if (!password.any { it.isLowerCase() }) {
+        println("Password must contain at least one lowercase letter!")
+    } else {
+        println("Password is valid")
+    }
+
+// Validate email not having a blank space
+    if (email.contains(" ")) {
+        println("Email cannot contain blank spaces!")
+    } else {
+        println("Email is valid")
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
+    }
 
 }
 
