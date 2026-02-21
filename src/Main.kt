1d02e7edeba7ffdb39f6c7b8ef4f6ee17190f25a
noire-er <xzlq9523@leeds.ac.uk>
2026-02-21 13:10:26 +0000

    mains

---
+++ src/Main.kt
@@ -0,0 +1,13 @@
+// want to add basic validation errors for website
+fun main() {
+    val email = "tester@example.com"
+    if (!email.contains()"@)) {
+        println("This is an Invalid email address: missing: @)
+    } else {
+        println("Email is valid")
+    }
+
+
+
+}
+
