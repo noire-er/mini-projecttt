c125dc6cc02c00a872589459ee5261a48b61fec1
noire-er <xzlq9523@leeds.ac.uk>
2026-02-22 13:49:37 +0000

    added some logic for backend

--- src/Main.kt
+++ src/Main.kt
@@ -1,27 +1,51 @@
-// want to add basic validation errors for website
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import io.ktor.server.application.*
+import io.ktor.server.routing.*
+import io.ktor.server.response.*
+import io.ktor.server.request.*
+import io.ktor.server.http.content.*
 
 fun main() {
-    val email = "tester@example.com"
-    if (!email.contains()"@)) {
-        println("This is an Invalid email address: missing: @)
-    } else {
-        println("Email is valid")
-    }
-// this is for missing '@' in an email address
-// Length validation
-    if (name.length < 2) {
-        println("Your name must be at least 2 letters!)
-    } else {
-        println("Name is valid")
-    }
+    embeddedServer(Netty, port = 8080) {
+        module()
+    }.start(wait = true)
+}
 
-    if (surname.length < 2) {
-        println("Your surname must be at least 2 letters!)
-    } else {
-        println("surname is valid")
+fun Application.module() {
+
+    routing {
+
+        // Serve static HTML files
+        staticResources("/", "static")
+
+        // Handle signup form
+        post("/signup") {
+
+            val params = call.receiveParameters()
+
+            val name = params["name"] ?: ""
+            val surname = params["surname"] ?: ""
+            val email = params["email"] ?: ""
+
+            // Validation
+            if (!email.contains("@")) {
+                call.respondText("Invalid email address: missing @")
+                return@post
+            }
+
+            if (name.length < 2) {
+                call.respondText("Name must be at least 2 letters")
+                return@post
+            }
+
+            if (surname.length < 2) {
+                call.respondText("Surname must be at least 2 letters")
+                return@post
+            }
+
+            call.respondText("Signup successful!")
+        }
     }
-
-// Numbers in name validation
-
 }
 
